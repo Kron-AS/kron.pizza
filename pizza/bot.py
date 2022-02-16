cde2c8e5c65411e5f20db30562d30d7086738070
Herman S <hermansc@users.noreply.github.com>
2022-02-16 19:09:01 +0100

    Use RTMClient instead of polling

--- pizza/bot.py
+++ pizza/bot.py
@@ -1,49 +1,35 @@
 #!/usr/bin/env python
 
 import os
-import sys
-from time import sleep
 
-from slackclient import SlackClient
+from slackclient import RTMClient
 
 from pizza import api
 
-slack_token = os.environ["SLACK_API_TOKEN"]
 
-sc = SlackClient(slack_token)
+@RTMClient.run_on(event='message.im')
+def on_message(**payload):
+    message = payload['data']
+    if not message["user"] in api.get_invited_users():
+        return
 
+    if message["text"].lower() == "ja":
+        api.rsvp(message["user"], "attending")
+        api.send_slack_message(message["channel"], "Sweet! 🤙")
+        api.finalize_event_if_complete()
 
-def is_dm(message):
-    return message["channel"][0] == "D"
+    elif message["text"].lower() == "nei":
+        api.rsvp(message["user"], "not attending")
+        api.send_slack_message(message["channel"], "Ok 😏")
+        api.invite_if_needed()
 
-
-if not sc.rtm_connect():
-    print("Connection Failed, invalid token?")
-    sys.exit(1)
-
-
-def main():
-    while True:
-        event_list = sc.rtm_read()
-        message_list = list(filter(lambda m: m["type"] == "message", event_list))
-        for message in message_list:
-            if is_dm(message) and "user" in message:
-                if message["user"] in api.get_invited_users():
-                    if message["text"].lower() == "ja":
-                        api.rsvp(message["user"], "attending")
-                        api.send_slack_message(message["channel"], "Sweet! 🤙")
-                        api.finalize_event_if_complete()
-                    elif message["text"].lower() == "nei":
-                        api.rsvp(message["user"], "not attending")
-                        api.send_slack_message(message["channel"], "Ok 😏")
-                        api.invite_if_needed()
-                    else:
-                        api.send_slack_message(
-                            message["channel"],
-                            "Hehe jeg er litt dum, jeg. Skjønner jeg ikke helt hva du mener 😳. Kan du være med? (ja/nei)",
-                        )
-        sleep(0.5)
+    else:
+        api.send_slack_message(
+            message["channel"],
+            "Hehe jeg er litt dum, jeg. Skjønner jeg ikke helt hva du mener 😳. Kan du være med? (ja/nei)",
+        )
 
 
 if __name__ == "__main__":
-    main()
+    rtm_client = RTMClient(token=os.environ["SLACK_API_TOKEN"])
+    rtm_client.start()
