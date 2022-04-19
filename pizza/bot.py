aecee782aa9a83d920582f20a58cf242b2285d58
Herman S <hermansc@users.noreply.github.com>
2022-04-19 11:07:00 +0200

    Translate slack messages

--- pizza/bot.py
+++ pizza/bot.py
@@ -32,16 +32,16 @@ def on_message(message, say, logger):
         )
         slack.send_slack_message(
             message["channel"],
-            "Hehe, jeg er teit og skjønner ikke hva du vil. Ikke tid for pizza nå? 😳",
+            "Hehe, I'm just a stupid robot and don't understand what you want. No pizza events planned at the moment? 😳",
         )
         return
 
-    if message["text"].lower() == "ja":
+    if message["text"].lower() == "yes":
         api.rsvp(message["user"], "attending")
         slack.send_slack_message(message["channel"], "Sweet! 🤙")
         api.finalize_event_if_complete()
 
-    elif message["text"].lower() == "nei":
+    elif message["text"].lower() == "no":
         api.rsvp(message["user"], "not attending")
         slack.send_slack_message(message["channel"], "Ok 😏")
         api.invite_if_needed()
@@ -49,7 +49,7 @@ def on_message(message, say, logger):
     else:
         slack.send_slack_message(
             message["channel"],
-            "Hehe jeg er litt dum, jeg. Skjønner jeg ikke helt hva du mener 😳. Kan du være med? (ja/nei)",
+            "Hehe I'm just a stupid robot and I don't understand what you mean 😳. Can you attend? (yes/no)",
         )
 
 
@@ -62,7 +62,9 @@ def handle_rsvp(ack, body, say, respond, logger):
     user_id = body["user"]["id"]
 
     if user_id not in api.get_invited_users():
-        say("💣 Hmm, hva har du gjort for noe rart nå?")
+        say(
+            "💣 Hmm, this is weird.. Something is wrong (you're not invited to any events) and I don't know what to do."
+        )
         return
 
     for action in body["actions"]:
@@ -72,11 +74,11 @@ def handle_rsvp(ack, body, say, respond, logger):
         if rsvp == "attending":
             logger.info("User is attending event [user=%s]", user_id)
             api.finalize_event_if_complete()
-            respond("✅ Sweet! Det blir sykt nice! 😋", replace_original=False)
+            respond("✅ Sweet! It'll be awesome! 😋", replace_original=False)
         elif rsvp == "not attending":
             logger.info("User is *not* attending event [user=%s]", user_id)
             api.invite_if_needed()
-            respond("⛔️ Ah, ok. Neste gang! 🤝", replace_original=False)
+            respond("⛔️ Ah, ok. Next time! 🤝", replace_original=False)
 
 
 if __name__ == "__main__":
