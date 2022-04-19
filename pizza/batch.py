496ff2a65f5dfde2d5cf981036d073461dcd6595
Herman S <hermansc@users.noreply.github.com>
2022-04-19 15:20:25 +0200

    Don't do anything after 8pm

--- pizza/batch.py
+++ pizza/batch.py
@@ -1,8 +1,26 @@
 #!/usr/bin/env python
 
 from pizza import api
+from pizza.utils import getLogger, tz_aware_now
 
-api.sync_db_with_slack()
-api.auto_reply()
-api.invite_if_needed()
-api.send_reminders()
+logger = getLogger(__name__)
+
+
+def main():
+    # Don't do anything in the evening/night
+    current_hour = tz_aware_now().hour
+    if current_hour >= 20 or current_hour < 8:
+        logger.info(
+            "Current hour %d is not during «business hours» so not doing anything",
+            current_hour,
+        )
+        return
+
+    api.sync_db_with_slack()
+    api.auto_reply()
+    api.invite_if_needed()
+    api.send_reminders()
+
+
+if __name__ == "__main__":
+    main()
